6fb2c049aa2d44987c7857d19011f020f30f17e5
Jack Collins <jackpaulcollins@gmail.com>
2019-07-18 19:30:04 -0700

    refactor works_controller to implement before_action & DRY code

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,39 +1,18 @@
 class WorksController < ApplicationController
+  before_action :set_work, only: [:show, :destroy, :update, :edit]
+
   def index
     @work_items = Work.all
+  end
+
+  def show
   end
 
   def new
     @work_item = Work.new
   end
 
-  def show
-    @work_item = Work.find(params[:id])
-  end
-
-  def destroy
-    @work_item = Work.find(params[:id])
-    @work_item.destroy
-    respond_to do |format|
-      format.html { redirect_to works_url, notice: 'Portfolio item was successfully destroyed.' }
-      format.json { head :no_content }
-  end
-
   def edit
-    @work_item = Work.find(params[:id])
-  end
-
-  def update
-    @work_item = Work.find(params[:id])
-    respond_to do |format|
-      if @work_item.update(work_params)
-        format.html { redirect_to works_path, notice: 'Portfolio piece was successfully updated.' }
-        format.json { render :show, status: :ok, location: @work }
-      else
-        format.html { render :edit }
-        format.json { render json: @work.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def create
@@ -50,7 +29,29 @@ class WorksController < ApplicationController
     end
   end
 
+  def destroy
+    @work_item.destroy
+    respond_to do |format|
+      format.html { redirect_to works_url, notice: 'Portfolio item was successfully destroyed.' }
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @work_item.update(work_params)
+        format.html { redirect_to works_path, notice: 'Portfolio piece was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def set_work
+    @work_item = Work.find(params[:id])
+  end
+
   def work_params
     params.require(:work).permit(:title, :subtitle, :body)
   end
 end
+
